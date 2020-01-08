2c6b0e8c25a20cb012eb45d23d5e13800aecb375
Jacob Archambault <jacob.archambault@waystar.com>
2020-01-08 08:30:49 -0500

    Remove unused usings and args parameter.

--- BasicConsoleIO/Program.cs
+++ BasicConsoleIO/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BasicConsoleIO
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("***** Basic Console I/O *****");
             GetUserData();
